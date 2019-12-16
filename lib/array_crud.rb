1ea81be8b812e15941812020bb28a53e7cbb9bbe
Ricky dildy <rdildy92@gmail.com>
2019-12-16 01:47:58 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -23,13 +23,13 @@ end
 def remove_element_from_end_of_array(array, element= "arrays!")
       array = ["wow", "I", "am", "really", "learning", "arrays!"]
       array.pop(element)
-      array "arrays!"
+      array ["arrays!"]
 end
 
 def remove_element_from_start_of_array(array, element= "wow")
      array= ["wow", "I", "am", "really", "learning", "arrays!"]
       array.shift(element)
-      array "wow"
+      array ["wow"]
 end
 
 def retrieve_element_from_index(array, index_number = "am")
