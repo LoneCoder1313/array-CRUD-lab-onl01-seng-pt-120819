90cb7ec428214098908773ee002c785f52aaeb95
Ricky dildy <rdildy92@gmail.com>
2019-12-16 01:46:48 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -22,7 +22,8 @@ end
 
 def remove_element_from_end_of_array(array, element= "arrays!")
       array = ["wow", "I", "am", "really", "learning", "arrays!"]
-      array.pop(element= "arrays!")
+      array.pop(element)
+      array "arrays!"
 end
 
 def remove_element_from_start_of_array(array, element= "wow")
