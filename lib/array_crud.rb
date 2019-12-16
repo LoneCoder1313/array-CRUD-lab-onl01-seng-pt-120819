869660cc3886572f61d16c64d3400fa7c270f13c
Ricky dildy <rdildy92@gmail.com>
2019-12-16 01:56:01 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -20,7 +20,7 @@ end
    end
    
 
-def remove_element_from_end_of_array(array, element="arrays!")
+def remove_element_from_end_of_array(array, element=5)
       array.pop(element)
       element==array("arrays!")
 end
