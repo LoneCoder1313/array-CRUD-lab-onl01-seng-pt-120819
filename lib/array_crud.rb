38d91b8e79ec1c7d7fad96b24d4f6088f079f0fe
Ricky dildy <rdildy92@gmail.com>
2019-12-16 01:06:04 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,35 +1,51 @@
-def create_an_empty_array
-  
+def create_an_empty_array 
+  []
 end
 
-def create_an_array
-  
+def create_an_array 
+  dog1 = "boxer"
+  dod2 = "pitbull"
+  dog3 = "mastiff"
+  dog4 = "corgi"
+  dog_breeds = ["boxer", "pitbull", "mastiff", "corgi"] 
 end
 
-def add_element_to_end_of_array(array, element)
-  
+def add_element_to_end_of_array(array = ["wow", "I", "am", "really", "learning"])
+    element = "arrays!"
+    while array =  ["wow", "I", "am", "really", "learning"]
+    array << element
+     array = ["wow", "I", "am", "really", "learning", "arrays!"]
+  end 
 end
 
-def add_element_to_start_of_array(array, element)
-  
-end
+def add_element_to_start_of_array(array, element) 
+       array = ["I", "am", "really", "learning", "arrays!"] 
+       array.unshift(element)
+   end
 
-def remove_element_from_end_of_array(array)
-  
+def remove_element_from_end_of_array(array, element)
+      array = ["wow", "I", "am", "really", "learning", "arrays!"]
+      array.pop("arrays!")
+        array = ["wow", "I", "am", "really", "learning"]
 end
 
 def remove_element_from_start_of_array(array)
-  
+    while  array = ["wow", "I", "am", "really", "learning", "arrays!"] do 
+      array.shift("wow")
+       array = ["I", "am", "really", "learning", "arrays!"]
+     end 
 end
 
-def retrieve_element_from_index(array, index_number)
-  
-end
+def retrieve_element_from_index(array, index_number = "am")
+      array = ["wow", "I", "am", "really", "learning", "arrays!"]
+      array.index(index_number)
+      index_number ="am"
+  end 
+      
 
 def retrieve_first_element_from_array(array)
-  
+      array = ["wow", "I", "am", "really", "learning", "arrays!"]
 end
 
 def retrieve_last_element_from_array(array)
-  
 end
